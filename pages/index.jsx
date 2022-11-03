3813a06b850daed98947d012665e9133a2f61067
robertsarafie <sarafierobert@gmail.com>
2022-11-03 19:59:17 +0200

    netlify commit - clasa

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,6 +1,10 @@
+
+
 export default function Home() {
-  return ( <div>
-    hello boston <i className="fa-solid fa-house"></i>
+  return (
+  <div>
+      hello boston <i className="fa-solid fa-house"></i>
+      <img src="/images/logo.jpg" alt="Alt" />
   </div>
   );
 }
