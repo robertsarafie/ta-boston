b7d3ac4b77e9782d3918661595fff880e43683f4
robertsarafie <sarafierobert@gmail.com>
2022-11-03 22:04:41 +0200

    ta-boston online - curs

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,10 +1,14 @@
-
-
 export default function Home() {
   return (
   <div>
       hello boston <i className="fa-solid fa-house"></i>
       <img src="/images/logo.jpg" alt="Alt" />
+      <button className="button button-primary">Button Primary</button>
+      <button className="button button-primary button-large">
+        Button Primary Large
+      </button>
+      <a className="button button-primary">Button Primary</a>
+      <a className="button button-primary button-large">Button Primary Large</a>
   </div>
   );
 }
